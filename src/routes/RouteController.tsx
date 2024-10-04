2424bd358fcb23efcfb08f7aa69779f150183209
dilshod2007 <dilshodumbarov707@gmail.com>
2024-10-04 11:33:03 +0500

    finished

--- src/routes/RouteController.tsx
+++ src/routes/RouteController.tsx
@@ -1,22 +1,35 @@
 import { Route, Routes } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useState, useEffect } from 'react';
+import { Spin } from 'antd';
 
 const ProductLikes = lazy(() => import('../routes/like/ProductLikes'));
-const Home= lazy(() => import('../routes/home/Home'));
+const Home = lazy(() => import('../routes/home/Home'));
 import Cart from '../routes/cart/Cart';
 
 const RouteController = () => {
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+    }, 2000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={
+      <div className="flex items-center justify-center h-screen">
+        {loading ? <Spin size="large" className='text-center' /> : null}
+      </div>
+    }>
       <Routes>
-        
         <Route path="/" element={<Home />} />
-        
         <Route path="/productLikes" element={<ProductLikes />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Suspense>
   );
-}
+};
 
 export default RouteController;
